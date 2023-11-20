a7365c73e5b45b788e6facee270a816bf6d3b7a7
DeevS <deev@advantum.pro>
2023-11-20 18:56:19 +0300

    Multi-project build

--- section2/src/main/java/com/example/section2/Section2Application.java
+++ section2/src/main/java/com/example/section2/Section2Application.java
@@ -1,13 +1,13 @@
-package com.example.springsecurity6;
+package com.example.section2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurity6Application {
+public class Section2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringSecurity6Application.class, args);
+        SpringApplication.run(Section2Application.class, args);
     }
 
 }
