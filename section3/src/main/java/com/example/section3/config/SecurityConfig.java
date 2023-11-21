1bbb70fde65fe2ba2d8875d53f758a893261720b
DeevS <deev@advantum.pro>
2023-11-21 19:15:39 +0300

    Section 3. InMemoryUserDetailsManager. Multiple in-memory users

--- section3/src/main/java/com/example/section3/config/SecurityConfig.java
+++ section3/src/main/java/com/example/section3/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.example.section3.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -27,6 +30,21 @@ public class SecurityConfig {
                 .httpBasic(withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .authorities("ADMIN")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("READ")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 
 }
