f3df954473c219a537c6734e1175b6da7c092b2f
DeevS <deev@advantum.pro>
2023-11-21 18:50:53 +0300

    Section 3. Init new subproject

---
+++ section3/src/main/java/com/example/section3/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.section3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(requests ->
+                        requests.requestMatchers(
+                                        "/accounts/**",
+                                        "/balance/**",
+                                        "/loans/**",
+                                        "/cards/**").authenticated()
+                                .requestMatchers(
+                                        "/contacts/**",
+                                        "/notices/**"
+                                ).permitAll())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+}
---
+++ section3/src/main/java/com/example/section3/controller/LoanController.java
@@ -0,0 +1,21 @@
+package com.example.section3.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/my-loans")
+    public String getLoans() {
+        return "Here are the loans details from the DB";
+    }
+
+}
