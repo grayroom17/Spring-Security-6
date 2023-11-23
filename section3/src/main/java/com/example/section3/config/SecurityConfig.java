e5b343b978d6ec92f855379ff6f7a12a0eca08d4
DeevS <deev@advantum.pro>
2023-11-23 20:02:58 +0300

    Section 3. JdbcUserDetailsManager. DB as user details storage.

--- section3/src/main/java/com/example/section3/config/SecurityConfig.java
+++ section3/src/main/java/com/example/section3/config/SecurityConfig.java
@@ -3,12 +3,13 @@ package com.example.section3.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -34,37 +35,44 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    InMemoryUserDetailsManager userDetailsManager() {
-        /**
-         * Approach 1 where we use withDefaultPasswordEncoder() method
-         * while creating the user details
-         */
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
+//    @Bean
+//    InMemoryUserDetailsManager userDetailsManager() {
+//        /**
+//         * Approach 1 where we use withDefaultPasswordEncoder() method
+//         * while creating the user details
+//         */
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("password")
+////                .authorities("ADMIN")
+////                .build();
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("12345")
+////                .authorities("READ")
+////                .build();
+////        return new InMemoryUserDetailsManager(admin, user);
+//
+//        /**
+//         * Approach 2 where we use NoOpPasswordEncoder Bean
+//         * while creating the user details
+//         */
+//        UserDetails admin = User.withUsername("admin")
 //                .password("password")
 //                .authorities("ADMIN")
 //                .build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
+//        UserDetails user = User.withUsername("user")
 //                .password("12345")
 //                .authorities("READ")
 //                .build();
 //        return new InMemoryUserDetailsManager(admin, user);
+//
+//
+//    }
 
-        /**
-         * Approach 2 where we use NoOpPasswordEncoder Bean
-         * while creating the user details
-         */
-        UserDetails admin = User.withUsername("admin")
-                .password("password")
-                .authorities("ADMIN")
-                .build();
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("READ")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
