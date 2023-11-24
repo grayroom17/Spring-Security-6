410e9f04e00e361b728aa69206a7703adfddbd38
DeevS <deev@advantum.pro>
2023-11-24 22:15:28 +0300

    Section 4. Password Encoders. BCryptPasswordEncoder. Hashing user password when create new user.

---
+++ section4/src/main/java/com/example/section4/controller/CardController.java
@@ -0,0 +1,21 @@
+package com.example.section4.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/cards")
+public class CardController {
+
+    @GetMapping("/my-cards")
+    public String getCards() {
+        return "Here are the cards details from the DB";
+    }
+
+}
