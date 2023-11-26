7fcb46cb445766e95cda27bc6862461dd4eb6c85
DeevS <deev@advantum.pro>
2023-11-26 10:37:01 +0300

    Section 6. Angular UI Application

---
+++ section6/00_UI_Initial Code/bank-app-ui/src/app/constants/app.constants.ts
@@ -0,0 +1,10 @@
+
+export const  AppConstants = {
+    CONTACT_API_URL : "/contacts",
+    LOGIN_API_URL : "/user",
+    ACCOUNT_API_URL : "/accounts/my-account",
+    BALANCE_API_URL : "/balance/my-balance",
+    LOANS_API_URL : "/loans/my-loans",
+    CARDS_API_URL : "/cards/my-cards",
+    NOTICES_API_URL : "/notices"
+}
