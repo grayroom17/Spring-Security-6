5cecc0ce38f07d93e3da9ae9b549051ecd932b29
DeevS <deev@advantum.pro>
2023-11-26 12:28:12 +0300

    Section 6. Backend application. Prepare migrations

---
+++ section6/src/main/java/com/example/section6/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example.section6.repository;
+
+import com.example.section6.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findAllByEmail(String email);
+
+}
