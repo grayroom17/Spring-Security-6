37e9e1b40cfddb1d1bdb925e420664066e56766f
DeevS <deev@advantum.pro>
2023-11-30 18:37:46 +0300

    Section 8. Enable Security FilterChain debug logging

--- section8/src/main/java/com/example/section8/Section8Application.java
+++ section8/src/main/java/com/example/section8/Section8Application.java
@@ -2,8 +2,10 @@ package com.example.section8;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class Section8Application {
 
     public static void main(String[] args) {
