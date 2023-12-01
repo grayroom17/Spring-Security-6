57de2dcf6d5f466a108fd5e73278bc65b6c436dc
DeevS <deev@advantum.pro>
2023-12-01 21:52:59 +0300

    Section 9. JWT. UI App. Pass Token to the request

--- bank-app-ui/src/app/components/login/login.component.ts
+++ bank-app-ui/src/app/components/login/login.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from "src/app/model/user.model";
-import { NgForm } from '@angular/forms';
-import { LoginService } from 'src/app/services/login/login.service';
-import { Router } from '@angular/router';
-import { getCookie} from "typescript-cookie";
+import {Component, OnInit} from '@angular/core';
+import {User} from "src/app/model/user.model";
+import {NgForm} from '@angular/forms';
+import {LoginService} from 'src/app/services/login/login.service';
+import {Router} from '@angular/router';
+import {getCookie} from "typescript-cookie";
 
 
 @Component({
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
   validateUser(loginForm: NgForm) {
     this.loginService.validateLoginDetails(this.model).subscribe(
       responseData => {
+        window.sessionStorage.setItem("Authorization", responseData.headers.get('Authorization')!)
         this.model = <any> responseData.body;
         let xsrf = getCookie('XSRF-TOKEN')!;
         window.sessionStorage.setItem("XSRF-TOKEN", xsrf)
