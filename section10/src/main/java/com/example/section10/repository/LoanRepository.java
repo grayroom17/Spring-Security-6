8a5f160d092e834d77a537eeac1753b70194f12a
DeevS <deev@advantum.pro>
2023-12-04 22:23:41 +0300

    Section 10. Method level Security. PreAuthorize annotation

--- section10/src/main/java/com/example/section10/repository/LoanRepository.java
+++ section10/src/main/java/com/example/section10/repository/LoanRepository.java
@@ -2,6 +2,7 @@ package com.example.section10.repository;
 
 import com.example.section10.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
+    @PreAuthorize("hasRole('USER')")
     List<Loan> findAllByCustomerIdOrderByStartedAtDesc(int customerId);
 
 }
