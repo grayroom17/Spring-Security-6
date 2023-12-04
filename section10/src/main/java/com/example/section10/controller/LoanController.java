1ab89604eee345b1c9d385bb84ef1b0156793ef5
DeevS <deev@advantum.pro>
2023-12-04 22:50:19 +0300

    Section 10. Method level Security. PostAuthorize annotation

--- section10/src/main/java/com/example/section10/controller/LoanController.java
+++ section10/src/main/java/com/example/section10/controller/LoanController.java
@@ -5,6 +5,7 @@ import com.example.section10.repository.LoanRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,7 @@ public class LoanController {
     LoanRepository loanRepository;
 
     @GetMapping("/my-loans")
+    @PostAuthorize("hasRole('USER')")
     public List<Loan> getLoanDetails(@RequestParam int id) {
         List<Loan> loans = loanRepository.findAllByCustomerIdOrderByStartedAtDesc(id);
         if (!loans.isEmpty()) {
