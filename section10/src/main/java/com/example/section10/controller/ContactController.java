d16d2d3ace825c77797e37873d78c0952d176131
DeevS <deev@advantum.pro>
2023-12-05 20:35:49 +0300

    Section 10. Method level Security. PreFilter annotation. Fix JwtValidationFilter

--- section10/src/main/java/com/example/section10/controller/ContactController.java
+++ section10/src/main/java/com/example/section10/controller/ContactController.java
@@ -5,12 +5,14 @@ import com.example.section10.repository.ContactRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.List;
 import java.util.Random;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -22,10 +24,12 @@ public class ContactController {
     ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    @PreFilter("filterObject.contactName != 'Test'")
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        Contact contact = contacts.getFirst();
         contact.setId(getServiceReqNumber());
         contact.setCreatedAt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        return List.of(contactRepository.save(contact));
     }
 
     public String getServiceReqNumber() {
