017ef414bef8bd29aaa233d1d07c6c49b9162721
DeevS <mygrayroom@gmail.com>
2023-12-08 19:35:11 +0300

    Section 11. OAuth2. Spring Boot OAuth2 client app

--- section12/src/main/java/com/example/section12/controller/dto/CustomerReadDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.section12.controller.dto;
-
-public record CustomerReadDto(Long id,
-                              String name,
-                              String email,
-                              String mobileNumber,
-                              String role) {
-
-}
--- section12/src/main/java/com/example/section12/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.section12.repository;
-
-import com.example.section12.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact, Long> {
-}
