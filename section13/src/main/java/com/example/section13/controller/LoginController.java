57f3547ef745f53a5e4d7b44cb108519b13ea432
DeevS <mygrayroom@gmail.com>
2023-12-12 19:07:25 +0300

    Section 13. Fixes

--- section13/src/main/java/com/example/section13/controller/LoginController.java
+++ section13/src/main/java/com/example/section13/controller/LoginController.java
@@ -1,21 +1,16 @@
 package com.example.section13.controller;
 
-import com.example.section13.model.Customer;
-import com.example.section13.controller.dto.CustomerCreateDto;
 import com.example.section13.controller.dto.CustomerReadDto;
 import com.example.section13.mapper.CustomerMapper;
+import com.example.section13.model.Customer;
 import com.example.section13.repository.CustomerRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.List;
 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -25,28 +20,6 @@ public class LoginController {
 
     CustomerRepository repository;
     CustomerMapper mapper;
-    PasswordEncoder passwordEncoder;
-
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody CustomerCreateDto dto) {
-        Customer customer = mapper.fromCreateDto(dto);
-
-        ResponseEntity response;
-        try {
-            String hashPassword = passwordEncoder.encode(customer.getPassword());
-            customer.setPassword(hashPassword);
-            customer.setCreatedAt(Date.valueOf(LocalDate.now()));
-            repository.save(customer);
-            response = ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(mapper.toReadDto(customer));
-        } catch (Exception e) {
-            response = ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception due to" + e.getMessage());
-        }
-        return response;
-    }
 
     @GetMapping("/user")
     public CustomerReadDto getUserDetailsAfterLogin(Authentication authentication) {
