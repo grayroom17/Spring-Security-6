12ee293c590ac239b6b4e93fa23eadf6d1059cbc
DeevS <mygrayroom@gmail.com>
2023-12-13 22:37:29 +0300

    Section 13. Authorization code flow with PKCE. Changes in UI app

--- bank-app-ui/src/app/model/account.model.ts
+++ bank-app-ui/src/app/model/account.model.ts
@@ -2,14 +2,14 @@
 export class Account {
 
   public customerId: number;
-  public id: number;
+  public accountNumber: number;
   public accountType: string;
   public branchAddress: string;
   
 
-  constructor(customerId?: number,id?: number,accountType?: string, branchAddress?: string){
+  constructor(customerId?: number,accountNumber?: number,accountType?: string, branchAddress?: string){
         this.customerId = customerId || 0;
-        this.id = id || 0;
+        this.accountNumber = accountNumber || 0;
         this.accountType = accountType || '';
         this.branchAddress = branchAddress || '';
   }
